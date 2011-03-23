6ea2ff5fcb55397313d3c8036358c8da5693b586
Grzegorz Nosek <root@localdomain.pl>
2011-03-23 16:44:31 +0100

    Use unannotated tags as a base of version number

--- version.py
+++ version.py
@@ -38,7 +38,7 @@ from subprocess import Popen, PIPE
 
 def call_git_describe(abbrev=4):
     try:
-        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
+        p = Popen(['git', 'describe', '--tags', '--abbrev=%d' % abbrev],
                   stdout=PIPE, stderr=PIPE)
         p.stderr.close()
         line = p.stdout.readlines()[0]
