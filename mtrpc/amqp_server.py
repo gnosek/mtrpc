306fe348527f09a16243f4204a46684229d7a211
Grzegorz Nosek <root@localdomain.pl>
2011-03-23 15:40:16 +0100

    amqp_server.py cleanups
     - properly reread config file after SIGHUP
     - remove same pointless sleep(0.5) as in mtrpc.server
     - remove (basically) useless --loop option

--- mtrpc/amqp_server.py
+++ mtrpc/amqp_server.py
@@ -3,6 +3,7 @@
 import sys
 import threading
 import time
+import signal
 import os
 from optparse import OptionParser
 
@@ -20,39 +21,27 @@ CONFIG_DIR = '/etc/megiteam/mtrpc'
 CONFIG_PATH = os.path.join(CONFIG_DIR, name + ".json")
 
 parser = OptionParser(usage='%prog [options]')
-parser.add_option('-l', '--loop', dest='loop', action='store_true', default=False, help='Loop inside MTRPC (internal)')
 parser.add_option('-d', '--daemon', dest='daemon', action='store_true', default=False, help='Daemonize')
 parser.add_option('-c', '--config', dest='config', default=CONFIG_PATH, help='Path to config file', metavar='FILE')
 
 (o, a) = parser.parse_args(sys.argv[1:])
 
-loop_mode = o.loop
 force_daemon = o.daemon
-config_dict = loader.load_props(open(o.config))
 
-if loop_mode:
-    final_callback = MTRPCServerInterface.restart_on
-    # (^ to restart the server when the service threads are stopped)
-    MTRPCServerInterface.configure_and_start(
-            config_dict=config_dict,
-            force_daemon=force_daemon,
-            loop_mode=True,       # <- stay in the inner server loop
-            final_callback=final_callback,
-    )
-else:
-    restart_lock = threading.Lock()
-    final_callback = restart_lock.release
-    # (^ to restart the server when the service threads are stopped)
-    try:
-        # no inner server loop needed, we have the outer one here
-        while True:
-            if restart_lock.acquire(False):   # (<- non-blocking)
-                server = MTRPCServerInterface.configure_and_start(
-                        config_dict=config_dict,
-                        force_daemon=force_daemon,
-                        loop_mode=False,  # <- return immediately
-                        final_callback=final_callback,
-                )
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        server.stop()
+restart_lock = threading.Lock()
+final_callback = restart_lock.release
+# (^ to restart the server when the service threads are stopped)
+try:
+    # no inner server loop needed, we have the outer one here
+    while True:
+        if restart_lock.acquire(False):   # (<- non-blocking)
+            config_dict = loader.load_props(open(o.config))
+            server = MTRPCServerInterface.configure_and_start(
+                    config_dict=config_dict,
+                    force_daemon=force_daemon,
+                    loop_mode=False,  # <- return immediately
+                    final_callback=final_callback,
+            )
+        signal.pause()
+except KeyboardInterrupt:
+    server.stop()
